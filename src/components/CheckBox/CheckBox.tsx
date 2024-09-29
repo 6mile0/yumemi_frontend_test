31ce2c0305fafa9ea74b6ebb7d20309ea5146db1
Kakeru Hokida <me@6mile.dev>
2024-09-29 10:53:55 +0900

    add: 都道府県の一覧を表示するモックを追加

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -3,23 +3,16 @@ import styles from "./CheckBox.module.css";
 
 type CheckBoxProps = {
   label: string;
-  checked: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const CheckBox: React.FC<CheckBoxProps> = ({
   label,
-  checked,
   onChange,
 }: CheckBoxProps) => {
   return (
     <label className={styles.label}>
-      <input
-        className={styles.input}
-        type="checkbox"
-        checked={checked}
-        onChange={onChange}
-      />
+      <input className={styles.input} type="checkbox" onChange={onChange} />
       {label}
     </label>
   );
